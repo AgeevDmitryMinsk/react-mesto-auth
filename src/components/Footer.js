40980d9a3ca58b54693c99bd8e09e3e9303e8aa0
daageev <ageev.dmitry@outlook.com>
2023-01-09 11:53:35 +0300

    update project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default Footer;
 function Footer() {
   return (
     <footer className="footer page__footer">
-      <p className="footer__copyright">{`© 2020 - ${new Date().getFullYear()} Mesto Russia AgeevDmitryMinsk`}</p>
+      <p className="footer__copyright">{`© 2022 - ${new Date().getFullYear()} Mesto AgeevDmitryMinsk`}</p>
     </footer>
   );
 }
